c4611cff6af96f6e8b07634f55e366ef5d1e1a5c
kesjuan <kesjuan@gmail.com>
2022-10-26 09:33:46 -0400

    springbootjoke

---
+++ src/main/java/com/jared/JokesAPI/JokesApiApplication.java
@@ -0,0 +1,13 @@
+package com.jared.JokesAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokesApiApplication.class, args);
+	}
+
+}
